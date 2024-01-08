13da48e0443cb90683c6cd8f44fd6a17c20563bc
Polantvic <polantvic@mail.ru>
2024-01-08 16:36:18 +0200

    0401 - 0402

--- 03_functions/albumas_lt.py
+++ 03_functions/albumas_lt.py
@@ -1,16 +1,35 @@
-""" Komandinio/individualaus darbo užduotis
-===[ Muzikos Albumas ]===
-
-Reikalavimai:
-
+"""
 * Žodynas albumas turi turėti atlikėją ir pavadinimą, gali turėti ir kitų atributų
-* Albumo žodyne sukurkite takelių (dainų) sąrašą, kur kiekvienas takelis yra žodynas, talpinantis eilės numerį, pavadinimą ir trukmę sekundėmis. 
+* Albumo žodyne sukurkite takelių (dainų) sąrašą, kur kiekvienas takelis yra žodynas,
+    talpinantis eilės numerį, pavadinimą ir trukmę sekundėmis. 
 ** Bonus: trukmės įvedimas "minutės:sekundės" formatu (žmogui suprantamu).
-* Programa turi leisti vartotojui užpildyti/pakeisti albumo informaciją (pavadinimą, atlikėją, ...)
-* Programa turi leisti vartotojui sukurti/ištrinti takelį, užpildant takelio informaciją (pavadinimą, trukmę)
-* Galimybė peržiūrėti albumą, išspausdinant takelių kiekį ir bendrą jų trukmę šalia kitų atributų.
-* Peržiūrėti albumo dainas. Bonus: išrūšiuotas pagal eilės numerį. Takelio trukmė turi būti pateikta žmogui suprantama laiko išraiška.
+* Programa turi leisti vartotojui užpildyti/pakeisti albumo informaciją (pavadinimą,
+    atlikėją, ...)
+* Programa turi leisti vartotojui sukurti/ištrinti takelį, užpildant takelio
+    informaciją (pavadinimą, trukmę)
+* Galimybė peržiūrėti albumą, išspausdinant takelių kiekį ir bendrą jų trukmę šalia
+    kitų atributų.
+* Peržiūrėti albumo dainas. Bonus: išrūšiuotas pagal eilės numerį. Takelio trukmė turi
+būti pateikta žmogui suprantama laiko išraiška.
 
 Pastabos:
-* Stenkitės nekartoti kodo - funkcionalumui, kuriam kodas kartotųsi, parašykite atskiras funkcijas ir jas panaudokite kelis kartus kur reikia.
+* Stenkitės nekartoti kodo - funkcionalumui, kuriam kodas kartotųsi, parašykite
+atskiras funkcijas ir jas panaudokite kelis kartus kur reikia.
+
+* Альбом словаря должен иметь исполнителя и имя, а также может иметь другие атрибуты.
+* Создать список треков (песен) в словаре альбома, где каждый трек представляет собой
+    словарь, содержащий порядковый номер, название и длительность в секундах.
+**Бонус: ввод продолжительности в формате «минута:секунда» (понятно человеку).
+* Приложение должно позволять пользователю заполнять/изменять информацию об альбоме
+    (название, исполнитель,...)
+* Приложение должно позволять пользователю создавать/удалять трек путем заполнения
+    информации о треке (название, продолжительность).
+* Возможность просмотра альбома, распечатав количество треков и их общую
+    продолжительность рядом с другими атрибутами.
+* Просмотр песен альбома. Бонус: сортировка по порядковому номеру. Длительность трека
+    должна быть представлена ​​в понятном человеку временном выражении.
+
+Примечания:
+* Старайтесь не повторять код — для функционала, для которого код будет повторяться,
+пишите отдельные функции и используйте их несколько раз там, где это необходимо.
 """
